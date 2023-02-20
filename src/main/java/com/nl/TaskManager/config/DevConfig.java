49aad2b11ee7871a907821786aa893d16179215f
MatheusBrito21 <matheusb790@gmail.com>
2023-02-20 19:41:48 -0300

    Perfis teste e dev

---
+++ src/main/java/com/nl/TaskManager/config/DevConfig.java
@@ -0,0 +1,24 @@
+package com.nl.TaskManager.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.nl.TaskManager.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	@Autowired
+	DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	String value;
+	
+	@Bean 
+	public void instanciaTesteDB() {
+		if(value.equals("create"))dbService.iniciaDB();
+	}
+}
