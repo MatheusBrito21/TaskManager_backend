23a6608f812b0777fae20483630a8a3ed20bef90
MatheusBrito21 <matheusb790@gmail.com>
2023-02-20 19:30:05 -0300

    Exception Handler

---
+++ src/main/java/com/nl/TaskManager/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.nl.TaskManager.controllers.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.nl.TaskManager.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> tarefaNaoEncontrada(ObjectNotFoundException ex, HttpServletRequest request){
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+				"Tarefa não encontrada!", ex.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+}
--- src/main/java/com/nl/TaskManager/services/TarefaService.java
+++ src/main/java/com/nl/TaskManager/services/TarefaService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.nl.TaskManager.dtos.TarefaDTO;
 import com.nl.TaskManager.model.Tarefa;
 import com.nl.TaskManager.repositories.TarefaRepository;
+import com.nl.TaskManager.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TarefaService {
@@ -23,7 +24,7 @@ public class TarefaService {
 
 	public Tarefa obterTarefa(Integer id) {
 		Optional<Tarefa> tarefa = tarefaRepository.findById(id);
-		return tarefa.orElseThrow(() -> new RuntimeException("Erro em achar tarefa"));
+		return tarefa.orElseThrow(() -> new ObjectNotFoundException("Informe um id cadastrado."));
 	}
 
 	public Tarefa cadastarTarefa(@Valid TarefaDTO tDTO) {
